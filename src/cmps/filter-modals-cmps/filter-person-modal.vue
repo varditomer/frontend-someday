8d60b286cc3e12f62e9ef9a0fa0003a3ac5724a5
varditomer <tomer.vardi@gmail.com>
2022-12-02 12:30:43 +0200

    filters modals

---
+++ src/cmps/filter-modals-cmps/filter-person-modal.vue
@@ -0,0 +1,23 @@
+<template>
+    <section class='filter-person-modal'>
+        <div class="modal-group upper-group flex column">
+            <div class="modal-item flex align-center">
+                <p class="title">Quick person filter</p>
+            </div>
+            <div class="modal-item flex align-center">
+                <p class="subtitle">Filter items and subitems by person</p>
+            </div>
+            <div class="modal-item flex align-center">
+                <!-- Todo: render persons and set filter by persons  -->
+            </div>
+        </div>
+
+    </section>
+</template>
+<script>
+export default {
+    name: 'filter-person-modal',
+    methods: {
+    }
+}
+</script>
