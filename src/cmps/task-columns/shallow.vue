da3d07516cdddb7ba52dfb12719848c7c44c7ec8
Ronen Boxer <ronenboxer@gmail.com>
2022-12-03 15:25:34 +0200

    task's text and numbers crud

--- src/cmps/task-columns/shallow.vue
+++ src/cmps/task-columns/shallow.vue
@@ -1,21 +1,36 @@
 <template>
-    <section>
-        
+    <section class="shallow">
+        <div  class="static" v-if="!isBeingEditted" @click="this.isBeingEditted = true">
+            {{ content ? content : '' }}
+        </div>
+        <div  class="dynamic" v-else>
+            <input type="number" v-focus :value="content" v-if="name === 'numbers'"
+                @blur="updateTask($event.target.value)" @keyup.enter="updateTask($event.target.value)" />
+            <textarea v-else v-focus @blur="updateTask($event.target.value)" :value="content"></textarea>
+        </div>
     </section>
 </template>
 
 <script>
 export default {
     name: '',
-    emits:['updateTask'],
+    emits: ['updateTask'],
     props: {
         content: String,
+        name: String
     },
     data() {
         return {
+            isBeingEditted: false,
         }
     },
+    created() {
+    },
     methods: {
+        updateTask(val) {
+            this.$emit('updateTask', { key: this.name, val })
+            this.isBeingEditted = false
+        }
     },
     computed: {
     },
