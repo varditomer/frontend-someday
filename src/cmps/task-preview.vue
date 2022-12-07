78798926f3d1632c01249b986009b9f35ce5c51c
varditomer <tomer.vardi@gmail.com>
2022-12-07 22:44:49 +0200

    fix all

--- src/cmps/task-preview.vue
+++ src/cmps/task-preview.vue
@@ -75,6 +75,7 @@ export default {
     created() {
         this.editing = false
         this.isChecked = false
+        console.log(`this.group:`, this.group)
     },
     data() {
         return {
