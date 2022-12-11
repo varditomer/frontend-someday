f616bf6c1f752c1e00dc468fe788209047a96a0a
Ronen Boxer <ronenboxer@gmail.com>
2022-12-11 11:45:45 +0200

    logged in user

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -129,6 +129,7 @@ function saveLocalUser(user) {
 function getLoggedinUser() {
     let loggedinUser = JSON.parse(sessionStorage.getItem(STORAGE_KEY_LOGGEDIN_USER))
     if (!loggedinUser) loggedinUser = { _id: '0', fullname: 'Guest', imgUrl: 'src/assets/imgs/default-avatar.svg' }
+    saveLocalUser(loggedinUser)
     return loggedinUser
 }
 
